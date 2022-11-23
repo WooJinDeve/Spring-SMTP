b60678b837da504b6a76d9d770c705d555e9f2a6
WoojinDeve <jwj060111@gmail.com>
2022-11-23 10:41:58 +0900

    feat: Auth Util class

--- src/main/java/com/example/mail/util/AuthEncoder.java
+++ src/main/java/com/example/mail/util/AuthEncoder.java
@@ -1,8 +1,18 @@
 package com.example.mail.util;
 
+import com.example.mail.global.exception.InvalidateException;
+import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AuthEncoder {
+    public String encode(final String password){
+        return BCrypt.hashpw(password, BCrypt.gensalt());
+    }
 
+    public boolean compare(final String password, final String encodePassword){
+        if (!BCrypt.checkpw(password, encodePassword))
+            throw new InvalidateException();
+        return true;
+    }
 }

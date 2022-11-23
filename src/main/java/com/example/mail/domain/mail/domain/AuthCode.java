de1fc5c43d61e34f6ef8cfa27664fb5734cf5e20
WoojinDeve <jwj060111@gmail.com>
2022-11-23 10:37:27 +0900

    feat: mail domain class

--- src/main/java/com/example/mail/domain/mail/domain/AuthCode.java
+++ src/main/java/com/example/mail/domain/mail/domain/AuthCode.java
@@ -1,17 +1,63 @@
 package com.example.mail.domain.mail.domain;
 
+import com.example.mail.global.exception.InvalidateException;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.time.LocalDateTime;
 
 import static javax.persistence.GenerationType.IDENTITY;
+import static lombok.AccessLevel.PROTECTED;
 
 @Getter
 @Entity
+@EntityListeners(AuditingEntityListener.class)
+@NoArgsConstructor(access = PROTECTED)
 public class AuthCode {
+    private static final Long EXPIRED_TIME = 10L;
 
     @Id @GeneratedValue(strategy = IDENTITY)
     private Long id;
+
+    private String code;
+
+    private String authSerialNumber;
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updateAt;
+
+    @Builder
+    public AuthCode(String code, String authSerialNumber, LocalDateTime createdAt, LocalDateTime updateAt) {
+        this.code = code;
+        this.authSerialNumber = authSerialNumber;
+        this.createdAt = createdAt;
+        this.updateAt = updateAt;
+    }
+
+    public static AuthCode of(String code, String authSerialNumber) {
+        return AuthCode.builder()
+                .code(code)
+                .authSerialNumber(authSerialNumber)
+                .build();
+    }
+
+    public void verify(String code){
+        if (!this.code.equals(code))
+            throw new InvalidateException();
+    }
+
+    public void verifyTime(LocalDateTime currentTime){
+        LocalDateTime expireTime = this.updateAt.plusMonths(EXPIRED_TIME);
+        if(currentTime.isAfter(expireTime))
+            throw new InvalidateException();
+    }
 }

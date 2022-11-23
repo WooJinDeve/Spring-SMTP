492724d21f97af99df4990de6467c0f266aaad70
WoojinDeve <jwj060111@gmail.com>
2022-11-23 10:46:26 +0900

    feat: mail sender class

--- src/main/java/com/example/mail/domain/mail/service/MailSender.java
+++ src/main/java/com/example/mail/domain/mail/service/MailSender.java
@@ -1,4 +1,5 @@
 package com.example.mail.domain.mail.service;
 
 public interface MailSender {
+    void sender(String email, String authCode);
 }
--- src/main/java/com/example/mail/domain/mail/service/MailSenderImpl.java
+++ src/main/java/com/example/mail/domain/mail/service/MailSenderImpl.java
@@ -1,7 +1,28 @@
 package com.example.mail.domain.mail.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.mail.MailProperties;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
 
 @Component
-public class MailSenderImpl {
+@RequiredArgsConstructor
+public class MailSenderImpl implements MailSender{
+
+    private static final String MAIL_SUBJECT_TEXT = "회원 가입 인증코드입니다.";
+
+    private final MailProperties properties;
+    private final JavaMailSender javaMailSender;
+
+    @Override
+    public void sender(String email, String authCode) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(properties.getUsername());
+        message.setTo(email);
+        message.setSubject(MAIL_SUBJECT_TEXT);
+        message.setText(authCode);
+
+        javaMailSender.send(message);
+    }
 }
